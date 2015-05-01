0a47286616c13f0a1f716ddaa204989a9819c601
Arjun Venkataswamy <avenkat2@gmail.com>
2015-05-01 09:51:15 -0500

    CRUDS out director resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
 
+  # CREATE
+  get "/directors/new", :controller => "directors", :action => "new", :as => "new_director"
+  post "/directors", :controller => "directors", :action => "create", :as => "directors"
+
+  # READ
+  get "/directors/:id", :controller => "directors", :action => "show", :as => "director"
+  get "/directors", :controller => "directors", :action => "index"
+
+  # UPDATE
+  get "/directors/:id/edit", :controller => "directors", :action => "edit", :as => "edit_director"
+  patch "/directors/:id", :controller => "directors", :action => "update"
+
+  # DESTROY
+  delete "/directors/:id", :controller => "directors", :action => "destroy"
+
 end
