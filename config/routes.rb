3b5b72815fa520f5dcf32d0f539b760d8c9b3b75
Arjun Venkataswamy <avenkat2@gmail.com>
2015-05-04 09:57:26 -0500

    dynamically display director name

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,36 @@
 Rails.application.routes.draw do
+  # Routes for the Director resource:
+  # CREATE
+  get '/directors/new',      :controller => 'directors', :action => 'new',    :as => 'new_director'
+  post '/directors',         :controller => 'directors', :action => 'create', :as => 'directors'
+
+  # READ
+  get '/directors',          :controller => 'directors', :action => 'index'
+  get '/directors/:id',      :controller => 'directors', :action => 'show',   :as => 'director'
+
+  # UPDATE
+  get '/directors/:id/edit', :controller => 'directors', :action => 'edit',   :as => 'edit_director'
+  patch '/directors/:id',    :controller => 'directors', :action => 'update'
+
+  # DELETE
+  delete '/directors/:id',   :controller => 'directors', :action => 'destroy'
+  #------------------------------
+
+  # Routes for the Movie resource:
+  # CREATE
+  get '/movies/new',      :controller => 'movies', :action => 'new',    :as => 'new_movie'
+  post '/movies',         :controller => 'movies', :action => 'create', :as => 'movies'
+
+  # READ
+  get '/movies',          :controller => 'movies', :action => 'index'
+  get '/movies/:id',      :controller => 'movies', :action => 'show',   :as => 'movie'
+
+  # UPDATE
+  get '/movies/:id/edit', :controller => 'movies', :action => 'edit',   :as => 'edit_movie'
+  patch '/movies/:id',    :controller => 'movies', :action => 'update'
+
+  # DELETE
+  delete '/movies/:id',   :controller => 'movies', :action => 'destroy'
+  #------------------------------
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150504142158) do
+
+  create_table "directors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image_url"
+    t.text     "bio"
+    t.string   "dob"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "year"
+    t.integer  "duration"
+    t.text     "description"
+    t.string   "image_url"
+    t.integer  "director_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
